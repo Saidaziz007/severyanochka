2b73b17e57fb06be0e03585341ed48222dc218ba
Saidaziz007 <saidaminovvs@gmail.com>
2024-01-26 01:57:47 +0500

    fourth commit

--- js/main.js
+++ js/main.js
@@ -6,10 +6,36 @@ let isOpen = false;
 toggle.addEventListener("click", () => {
   isOpen = !isOpen;
   if (isOpen) {
-    menu.style.top = "70px";
+    menu.style.top = "50px";
     header.style.boxShadow = "none";
   } else {
-    menu.style.top = "-250px";
+    menu.style.top = "-350px";
     header.style.boxShadow = "0 0 5px 5px rgb(137, 137, 137)";
   }
+});
+
+const tabButtons = document.querySelectorAll(".shop__location button");
+const tabContents = document.querySelectorAll(".shop__bottom__loc");
+
+let active = 0;
+
+function getTabContents() {
+  tabContents.forEach((el, i) => {
+    if (i == active) {
+      el.style.display = "block";
+      tabButtons[i].classList.add("active-tab");
+    } else {
+      el.style.display = "none";
+      tabButtons[i].classList.remove("active-tab");
+    }
+  });
+}
+
+getTabContents();
+
+tabButtons.forEach((el, i) => {
+  el.addEventListener("click", function () {
+    active = i;
+    getTabContents();
+  });
 });
